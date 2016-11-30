2efed12b0ac7445cb85e6856f5d948f3a9ae22e0
yatharth <yatharths@iiit.ac.in>
2016-11-30 16:04:14 -0700

    added filters

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,9 +11,22 @@ class DashboardController < ApplicationController
 		# 	{"score" => {"$meta" =>'textScore'}}).limit(2)
 
 		#todo sort the above row
-		# .sort({"score" => {"$meta" =>  'textScore'}}).
-		
+		# # .sort({"score" => {"$meta" =>  'textScore'}}).
+		# @code_sim = Stackoverflow.collection.aggregate(
+		# 	[{"$match" =>  {"$text" => {"$search"  =>  params['data']}}},
+		# 	{"$project" => {"tags_array" => 1,"title" => 1,"type" => 1,"vote" => 1,"code" => 1,"score" => {"$meta" => "textScore"},"_id" =>  0}},
+		# 	{"$match" => {"score"  => {"$gt" =>  1 }}},
+		# 	{"$unwind" => "$tags_array"},
+		# 	{"$group" => 
+		# 	{"_id" => {"question" => "$title", "key" => "$code", "tags" =>  "$tags_array"},"value" => {"$sum" => "$vote"}}},
+		# 	{"$project" => {"question"  => "$_id.question","key"  => "$_id.key","tags"  => "$_id.tags","value" => "$value"}},
+		# 	{"$sort" => {"value" => -1}}
+		# 	],{:allow_disk_use => true}) 
 
+
+ 	# 	respond_to do |format|
+	 #      format.json { render json: @code_sim.first}
+	 #    end
 		# all_tags = []
 		# @tags.each do |data|
 		#  data['tag'].split(" ").each do |new_tag|
@@ -27,59 +40,69 @@ class DashboardController < ApplicationController
   end
 
 
+	def treemap
+		@code_sim = Stackoverflow.collection.aggregate(
+			[{"$match" =>  {"$text" => {"$search"  =>  params['data']}}},
+			{"$project" => {"tags_array" => 1,"title" => 1,"type" => 1,"value" => 1,"vote"=>1,"code" => 1,"score" => {"$meta" => "textScore"},"_id" =>  0}},
+			{"$match" => {"score"  => {"$gt" =>  1 }}},
+			{"$sort" => {"score" => -1}},
+			{"$limit" => 50},
+			{"$unwind" => "$tags_array"},
+			{"$group" => 
+			{"_id" => {"question" => "$title", "key" => "$code", "tags" =>  "$tags_array", "value"=>"$value"},"vote"=> { "$first" => "$vote"}}},
+			{"$project" => {"question"  => "$_id.question","key"  => "$_id.key","tag"  => "$_id.tags","value" => "$_id.value", "vote"=>"$vote"}},
+			
+			],{:allow_disk_use => true}) 
+
+
+		# @code_sim.each do |data|
+		# 	print data, "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@",'\n'
+		# end
+ 		respond_to do |format|
+	      format.json { render json: @code_sim}
+	    end
+
+	end
+
   def fetch_n_gram
 	filter_hash = {}
 
 		tree = Hash.new
 	    data_arr = []
 
-	    @filtered_data = Finalngram.collection.aggregate(
+	 
+    @filtered_data = Finalngram.collection.aggregate(
 	    	[{"$match" =>{"word" => params['word1']}},
+
+	    	{"$group" => {"_id" => {"word" =>  "$word", "n1" =>  "$n1","n2" =>  "$n2","n3" =>  "$n3"}, 
+			"count" => {"$sum" => 1}, "parent" => {"$push" => "$$ROOT"}}},
+			{"$sort" => {"count" => -1}},
+			{"$project" => {"_id" => 0,"word" =>  "$_id.word","n1" => "$_id.n1","n2" => "$_id.n2","name" => "$_id.n3","size" => "$count"}},
+			{"$limit" => 50},
 
 			{"$group" =>{"_id" =>{"word" => "$word", "n1" => "$n1","n2" => "$n2"}, 
 			"count" => {"$sum" =>1}, "parent" =>{"$push" => "$$ROOT"}}},
 			{"$sort"  =>{"count" => -1}},
-			{"$project" =>{"_id" =>0,"word" => "$_id.word","n1" => "$_id.n1","name" => "$_id.n2","size" => "$count"}},
-			{"$limit" => 25},
+			{"$project" =>{"_id" =>0,"word" => "$_id.word","n1" => "$_id.n1","name" => "$_id.n2","size" => "$count","children" => "$parent"}},
+			{"$limit" => 50},
 
 			{"$group" =>{"_id" =>{"word" => "$word", "n1" => "$n1"}, 
 			"count" => {"$sum" =>1}, "parent" =>{"$push" => "$$ROOT"}}},
 			{"$sort"  =>{"count" => -1}},
-			{"$project" =>{"_id" =>0,"word" => "$_id.word","name" => "$_id.n1","count" => "$count","children" => "$parent"}},
-			{"$limit" => 25},
+			{"$project" =>{"_id" =>0,"word" => "$_id.word","name" => "$_id.n1","size" => "$count","children" => "$parent"}},
+			{"$limit" => 50},
 
 			{"$group" =>{"_id" =>{"word" => "$word"}, 
 			"count" => {"$sum" =>1}, "parent" =>{"$push" => "$$ROOT"}}},
 			{"$sort"  =>{"count" => -1}},
-			{"$project" =>{"_id" =>0,"name" => "$_id.word","count" => "$count","children" => "$parent"}},
+			{"$project" =>{"_id" =>0,"name" => "$_id.word","size" => "$count","children" => "$parent"}},
 
-			{"$limit" => 25}],
+			{"$limit" => 50}],
 
 			{ :allow_disk_use => true}
 			)
 
 
-	    # print   @filtered_data.to_json
-	    # print "$$$$$$$$$$$$$$$$$$$$$$$", params['word1']
-	  	# @filtered_data = Finalngram.collection.aggregate([
-	  	# 	{"$match" => {"word" => "int"}},
-	  	# 	{"$group" => {"_id" =>{"n1" =>  "$n1"},"count" => {"$sum" => 1}}},
-	  	# 	{"$sort" => { "count"  => -1}},
-	  	# 	{"$limit": 200},
-	  	# 	{"$project":{"_id":0,'name':  "$_id.n1",count: "$count"}}],
-	  	# 	{ allowDiskUse: true})
-
-	  	# print "@@@@@@@@@@@@@@@@@", @filtered_data.count
-
-	  		# tree['name'] = @filtered_data
-	  	# 	tree['children'] = Array.new
-	  	# 	@filtered_data.each do |data|
-	  	# 		# print data['name']
-	  	# 		data_arr << data['name']
-				# tree['children'] << {"name" => data['name'] , "size" =>data['count'], "children" => Array.new}
-	  	# 	end
-
-	
 
 
 	  	respond_to do |format|
@@ -87,6 +110,7 @@ class DashboardController < ApplicationController
 	    end
  
 	end
+
 end
 
 
